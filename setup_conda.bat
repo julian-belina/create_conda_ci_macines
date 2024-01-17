01d964447f13e85784672dc931cf5270a6114c93
j.belina <j.belina@fz-juelich.de>
2024-01-17 03:30:41 +0100

    asd

--- setup_conda.bat
+++ setup_conda.bat
@@ -1,3 +1,5 @@
 curl.exe --output Miniforge.exe --url https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Windows-x86_64.exe -L
 dir
-Miniforge.exe /InstallationType=JustMe /RegisterPython=0 /S /AddToPath=1 /D=./miniforge
+start /wait "" Miniforge.exe /InstallationType=JustMe /RegisterPython=1 /S /AddToPath=1 /D=%0\miniforge
+conda -V
+mamba -V
